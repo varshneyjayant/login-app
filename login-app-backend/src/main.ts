17560f7440744d4e2e84dce8626da6a692672f3e
Jayant Varshney <jayantvarshney018@gmail.com>
2021-03-18 20:15:28 +0530

    backend:add cors options

--- login-app-backend/src/main.ts
+++ login-app-backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.UI_HOST,
+  });
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
