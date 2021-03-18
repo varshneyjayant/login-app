aa986b52c4b137ce00febf9ff44f1a68b4211511
Jayant Varshney <jayantvarshney018@gmail.com>
2021-03-18 20:17:22 +0530

    frontend:add config, api urls

---
+++ login-app-frontend/src/app/config/messages.ts
@@ -0,0 +1,7 @@
+export const MESSAGES = {
+  MISSING_USERNAME: "Username is required",
+  MISSING_PASSWORD: "Password is required",
+  PASSWORD_LENGHT_INCORRECT: "Passowrd must be 6 to 20 charactes.",
+  AUTHENTICATION_ERROR: "Invalid credentials. Please try again",
+  UNKNOWN_ERROR: "Unknown error. Please try again",
+};
